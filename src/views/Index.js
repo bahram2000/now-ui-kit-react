10dd87a852df60f91b6e2f70aa0b0a6344fbd6cb
Bahram <50679501+bahram2000@users.noreply.github.com>
2024-04-26 22:00:13 +0330

    Update Index.js

--- src/views/Index.js
+++ src/views/Index.js
@@ -42,22 +42,7 @@ function Index() {
       <IndexNavbar />
       <div className="wrapper">
         <IndexHeader />
-        // <div className="main">
-        //   <Images />
-        //   <BasicElements />
-        //   <Navbars />
-        //   <Tabs />
-        //   <Pagination />
-        //   <Notifications />
-        //   <Typography />
-        //   <Javascript />
-        //   <Carousel />
-        //   <NucleoIcons />
-        //   <CompleteExamples />
-        //   <SignUp />
-        //   <Examples />
-        //   <Download />
-        // </div>
+  
         <DarkFooter />
       </div>
     </>
